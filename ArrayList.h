267cd779f3bb92e47e8595eb646996e009ba5072
Carlos <carlosorchavez25@gmail.com>
2025-09-22 23:24:45 -0400

    Add: Delete, remove and menu

--- ArrayList.h
+++ ArrayList.h
@@ -6,16 +6,13 @@
 using namespace std;
 
 template <typename T>
-
 class ArrayList {
-
 private:
     T* data;
     int capacity;
     int count;
 
 public:
-
     ArrayList(int initialCapacity = 10) {
         capacity = initialCapacity;
         data = new T[capacity];
@@ -41,32 +38,45 @@ public:
         data[count++] = Object;
     }
 
-    T get (int index) const {
-          if (index < 0 || index >= count) {
+    T get(int index) const {
+        if (index < 0  index >= count) {
             throw std::out_of_range("Index out of bounds");
         }
         return data[index];
     }
 
-    void set(int index,const T& object){
-        if (index < 0 || index >= count) {
+    void set(int index, const T& object) {
+        if (index < 0  index >= count) {
             throw std::out_of_range("Index out of bounds");
         }
         data[index] = object;
     }
 
-    T remove(T index){
-
+    T remove(int index) {
+        if (index < 0  index >= count) {
+            throw std::out_of_range("Index out of bounds");
+        }
+        T removeElement = data[index];
+        for (int i = index; i < count - 1; ++i) {
+            data[i] = data[i + 1];
+        }
+        count--;
+        return removeElement;
     }
 
-    void Delete(T index){
-
+    void Delete(int index) {
+        if (index < 0  index >= count) {
+            throw std::out_of_range("Index out of bounds");
+        }
+        for (int i = index; i < count - 1; ++i) {
+            data[i] = data[i + 1];
+        }
+        count--;
     }
 
     int size() const {
         return count;
     }
-
 };
 
 #endif // ARRAYLIST_H
--- main.cpp
+++ main.cpp
@@ -1,13 +1,60 @@
 #include <iostream>
+#include <string>
 #include "ArrayList.h"
 
 using namespace std;
 int main() {
-    ArrayList<int> list;
-    list.add(10);
-    list.add(20);
-    list.add(30);
+    ArrayList<string> list;
+    int opcion, index;
+    string valor;
 
+    do {
+        cout << "Menu de opciones:\n";
+        cout << "1. Agregar valor\n";
+        cout << "2. Obtener valor por indice\n";
+        cout << "3. Eliminar valor por indice\n";
+        cout << "4. Mostrar tamano de la lista\n";
+        cout << "5. Salir\n";
+        cout << "Seleccione una opcion: ";
+        cin >> opcion;
+
+        switch (opcion) {
+            case 1:
+                cout << "Ingrese un valor para agregar: ";
+                cin.ignore();
+                getline(cin, valor);
+                list.add(valor);
+                break;
+            case 2:
+                cout << "Ingrese el indice del valor a obtener: ";
+                cin >> index;
+                try {
+                    cout << "Valor en el indice " << index << ": " << list.get(index) << endl;
+                } catch (const out_of_range& e) {
+                    cout << e.what() << endl;
+                }
+                break;
+            case 3:
+                cout << "Ingrese el indice del valor a eliminar: ";
+                cin >> index;
+                try {
+                    list.Delete(index);
+                    cout << "Valor eliminado." << endl;
+                } catch (const out_of_range& e) {
+                    cout << e.what() << endl;
+                }
+                break;
+            case 4:
+                cout << "Tamano de la lista: " << list.size() << endl;
+                break;
+            case 5:
+                cout << "Saliendo..." << endl;
+                break;
+            default:
+                cout << "Opcion invalida." << endl;
+        }
+        cout << endl;
+    } while (opcion != 5);
 
     return 0;
 }
